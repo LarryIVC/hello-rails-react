702ccd9da0841c62f03a33e5ceb6e609915ab50b
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-09-05 00:47:18 -0400

    Consuming the API

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,40 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
+const API = 'http://127.0.0.1:3000/api/random_greeting';
+
+
+function Greeting() {
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setGreeting(data.greeting);
+      });
+  }, []);
+
+  return (
+    <h2>Your greeting is: {greeting}</h2>
+  )
+}
+
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <>
+      <h1>Hello, React on Rails!</h1>
+      <Greeting />
+    </>
+  );
 }
 
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root'),
 );
